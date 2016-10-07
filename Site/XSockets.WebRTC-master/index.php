ff532e317876230c8ed25d991e06440e00161c5f
Conor Thompson <x13466188@student.ncirl.ie>
2016-10-07 14:10:15 +0100

    added in site layout

--- Site/XSockets.WebRTC-master/index.php
+++ Site/XSockets.WebRTC-master/index.php
@@ -1,29 +1,62 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Emergency Consultants</title>
-    <meta charset="utf-8" />
-    <link href="css/css.css" rel="stylesheet" />
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Grayscale - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+
+    <!-- Theme CSS -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 </head>
-<body>
-    <h1>XSockets.WebRTC Client example </h1>
-    <div class="localvideo">
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">Grayscale</h1>
+                        <p class="intro-text">A free, responsive, one page Bootstrap theme.
+                            <br>Created by Start Bootstrap.</p> <div class="localvideo">
         <video autoplay></video>
     </div>
 
     <h2>Remote videos</h2>
-    <div class="remotevideos">
+    <div class="remotevideos"><div id="immediate"></div>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-    </div>
-    <h2>Recordings  ( Click on your camera stream to start record)</h2>
-    <ul></ul>
+   
 
-
-    <h2>Trace</h2>
-    <div id="immediate"></div>
-    <script src="lib/XSockets.latest.js"></script>
+    <!-- jQuery -->	
+	    <script src="lib/XSockets.latest.js"></script>
     <script src="xsockets/js/XSockets.WebRTC.latest.js"></script>
     <script>
         var $ = function (selector, el) {
@@ -125,5 +158,6 @@
         });
         document.addEventListener("DOMContentLoaded", main);
     </script>
+
 </body>
 </html>
